82b16896bc5aeeca075dfe892c637221d77ac90b
florian.helg <florian.helg@iut-rodez.fr>
2025-05-23 14:46:47 +0200

    Ajout de la fonctionnalité CommentJouer qui explique les règles du jeu sur l'utilisateur clique sur ce bouton. Il ne s'agit pas de la dernière version car il manque les boutons de retour qui ne marchent pas, une nouvelle version arrive soon

---
+++ src/application/controller/CommentJouerController.java
@@ -0,0 +1,54 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommentJouerController {
+
+    @FXML
+    private StackPane mainPane;
+
+    @FXML
+    private AnchorPane page1, page2, page3, page4, page5, page6, page7, page8, page9, page10, page11, page12, page13, page14, page15;
+
+    private List<AnchorPane> pages;
+    private int currentPage = 0;
+
+    @FXML
+    public void initialize() {
+        pages = Arrays.asList(page1, page2, page3, page4, page5, page6, page7, page8, page9, page10, page11, page12, page13, page14, page15);
+        showPage(currentPage);
+    }
+
+    private void showPage(int index) {
+        for (int i = 0; i < pages.size(); i++) {
+            pages.get(i).setVisible(i == index);
+        }
+    }
+
+    @FXML
+    private void handleNext() {
+        if (currentPage < pages.size() - 1) {
+            currentPage++;
+            showPage(currentPage);
+        }
+    }
+
+    @FXML
+    private void handlePrev() {
+        if (currentPage > 0) {
+            currentPage--;
+            showPage(currentPage);
+        }
+    }
+}
